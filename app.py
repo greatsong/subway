0075d22e6bd5e6a022091da670c929438a0f93ef
송석리(Song Sukree) <greatsong21@gmail.com>
2024-06-25 07:18:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import koreanize_matplotlib
 
 # 데이터 로드
 csv_file_path = '2024년 05월  교통카드 통계자료.csv'
-subway_data = pd.read_csv(csv_file_path)
-
+subway_data = pd.read_csv(csv_file_path)import streamlit as st
 
 # 데이터 전처리
 subway_data_cleaned = subway_data.drop(0).reset_index(drop=True)
@@ -22,14 +21,16 @@ for column in columns_to_convert:
 
 # 시간대별 승차 인원 최다 역 찾기
 boarding_max_stations = subway_data_cleaned.loc[subway_data_cleaned[columns_to_convert[::2]].idxmax()]
+boarding_max_counts = subway_data_cleaned[columns_to_convert[::2]].max().values
+boarding_max_station_names = boarding_max_stations['지하철역'].values
 
 # 시간대별 하차 인원 최다 역 찾기
 alighting_max_stations = subway_data_cleaned.loc[subway_data_cleaned[columns_to_convert[1::2]].idxmax()]
+alighting_max_counts = subway_data_cleaned[columns_to_convert[1::2]].max().values
+alighting_max_station_names = alighting_max_stations['지하철역'].values
 
 # 시간대별 그래프 생성
 time_periods = [col.split('~')[0][:2] for col in columns_to_convert[::2]]  # 시간대 2자리로 축약
-boarding_max_counts = subway_data_cleaned[columns_to_convert[::2]].max().values
-alighting_max_counts = subway_data_cleaned[columns_to_convert[1::2]].max().values
 
 # 시간대별 승차 인원 최다 역 그래프 그리기
 fig, ax = plt.subplots()
@@ -43,6 +44,10 @@ ax.set_title('시간대별 승차 인원 최다 역')
 ax.set_xticks([p for p in x])
 ax.set_xticklabels(time_periods, rotation=90)
 ax.legend()
+
+# 역 이름을 아래 방향으로 표시
+for i, v in enumerate(boarding_max_counts):
+    ax.text(i, v + 100, str(boarding_max_station_names[i]), ha='center', rotation=90)
 
 st.pyplot(fig)
 
@@ -58,5 +63,9 @@ ax.set_title('시간대별 하차 인원 최다 역')
 ax.set_xticks([p for p in x])
 ax.set_xticklabels(time_periods, rotation=90)
 ax.legend()
+
+# 역 이름을 아래 방향으로 표시
+for i, v in enumerate(alighting_max_counts):
+    ax.text(i, v + 100, str(alighting_max_station_names[i]), ha='center', rotation=90)
 
 st.pyplot(fig)
