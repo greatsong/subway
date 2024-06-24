9323fa0c8ae6554185ce74c707b0e8e35dde27f1
송석리(Song Sukree) <greatsong21@gmail.com>
2024-06-25 07:16:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import koreanize_matplotlib
 csv_file_path = '2024년 05월  교통카드 통계자료.csv'
 subway_data = pd.read_csv(csv_file_path)
 
+
 # 데이터 전처리
 subway_data_cleaned = subway_data.drop(0).reset_index(drop=True)
 
@@ -19,41 +20,42 @@ columns_to_convert = subway_data_cleaned.columns[4:]
 for column in columns_to_convert:
     subway_data_cleaned[column] = clean_numeric_column(subway_data_cleaned[column])
 
-# 전체 시간대에 대해 승하차 인원이 가장 많은 역 찾기
-subway_data_cleaned['총 승차'] = subway_data_cleaned[columns_to_convert[::2]].sum(axis=1)
-subway_data_cleaned['총 하차'] = subway_data_cleaned[columns_to_convert[1::2]].sum(axis=1)
-subway_data_cleaned['총 승하차'] = subway_data_cleaned['총 승차'] + subway_data_cleaned['총 하차']
-busiest_station = subway_data_cleaned.loc[subway_data_cleaned['총 승하차'].idxmax()]
+# 시간대별 승차 인원 최다 역 찾기
+boarding_max_stations = subway_data_cleaned.loc[subway_data_cleaned[columns_to_convert[::2]].idxmax()]
+
+# 시간대별 하차 인원 최다 역 찾기
+alighting_max_stations = subway_data_cleaned.loc[subway_data_cleaned[columns_to_convert[1::2]].idxmax()]
 
 # 시간대별 그래프 생성
 time_periods = [col.split('~')[0][:2] for col in columns_to_convert[::2]]  # 시간대 2자리로 축약
-boardings = busiest_station[columns_to_convert[::2]].values
-alightings = busiest_station[columns_to_convert[1::2]].values
+boarding_max_counts = subway_data_cleaned[columns_to_convert[::2]].max().values
+alighting_max_counts = subway_data_cleaned[columns_to_convert[1::2]].max().values
 
-# 데이터 길이 일치 확인 및 처리
-if len(alightings) < len(time_periods):
-    alightings = list(alightings) + [0] * (len(time_periods) - len(alightings))
-
-# 가장 바쁜 역 이름
-busiest_station_name = busiest_station['지하철역']
-
-# 시간대와 승하차 데이터 길이 확인
-st.write(f'Time Periods Length: {len(time_periods)}')
-st.write(f'Boardings Length: {len(boardings)}')
-st.write(f'Alightings Length: {len(alightings)}')
-
-# 막대 그래프 그리기
+# 시간대별 승차 인원 최다 역 그래프 그리기
 fig, ax = plt.subplots()
 width = 0.35  # 막대의 너비
 x = range(len(time_periods))
 
-ax.bar(x, boardings, width, label='승차')
-ax.bar([p + width for p in x], alightings, width, label='하차')
-
+ax.bar(x, boarding_max_counts, width, label='승차 인원 최다 역')
 ax.set_xlabel('시간대')
 ax.set_ylabel('인원수')
-ax.set_title(f'{busiest_station_name} 시간대별 승하차 인원수')
-ax.set_xticks([p + width/2 for p in x])
+ax.set_title('시간대별 승차 인원 최다 역')
+ax.set_xticks([p for p in x])
+ax.set_xticklabels(time_periods, rotation=90)
+ax.legend()
+
+st.pyplot(fig)
+
+# 시간대별 하차 인원 최다 역 그래프 그리기
+fig, ax = plt.subplots()
+width = 0.35  # 막대의 너비
+x = range(len(time_periods))
+
+ax.bar(x, alighting_max_counts, width, label='하차 인원 최다 역')
+ax.set_xlabel('시간대')
+ax.set_ylabel('인원수')
+ax.set_title('시간대별 하차 인원 최다 역')
+ax.set_xticks([p for p in x])
 ax.set_xticklabels(time_periods, rotation=90)
 ax.legend()
 
